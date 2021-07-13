1183b15b13395ce779a1d9e7e054fc395e2c5274
HumzaHemani <humzah@umich.edu>
2021-07-13 18:43:14 -0400

    Update step11_GenerateStatsFromSingleCellBAM.sh

--- step11_GenerateStatsFromSingleCellBAM.sh
+++ step11_GenerateStatsFromSingleCellBAM.sh
@@ -17,15 +17,18 @@ done
 
 #set -o xtrace
 
-echo arguments read in
-READ_EXOME=$(samtools view -c -F 4 -L ${Targets} ${DataDirectory}/${Sample})
-echo read_exome done
-READ_SAM=$(samtools view -c -F 4 ${DataDirectory}/${Sample})
-echo read_sam done
-UMI=$(samtools view  ${DataDirectory}/${Sample} | grep -o \'UB:............\' | grep -o \'..........$\' | uniq -c | wc -l)
-echo umi done
-COV_EXOME=$(samtools depth -b ${Targets} ${DataDirectory}/${Sample} | wc -l)
-echo cov_exome done
-COV=$(samtools depth ${DataDirectory}/${Sample} | wc -l)
-echo cov done
-echo $Sample, $READ_EXOME, $READ_SAM, $UMI, $COV_EXOME, $COV >> ${Outdir}/mutations.csv
+for Sample in ${DataDirectory}/*.bam
+do
+    echo arguments read in
+    READ_EXOME=$(samtools view -c -F 4 -L ${Targets} ${Sample})
+    echo read_exome done
+    READ_SAM=$(samtools view -c -F 4 ${Sample})
+    echo read_sam done
+    UMI=$(samtools view  ${Sample} | grep -o \'UB:............\' | grep -o \'..........$\' | uniq -c | wc -l)
+    echo umi done
+    COV_EXOME=$(samtools depth -b ${Targets} ${Sample} | wc -l)
+    echo cov_exome done
+    COV=$(samtools depth ${Sample} | wc -l)
+    echo cov done
+    echo $Sample, $READ_EXOME, $READ_SAM, $UMI, $COV_EXOME, $COV >> ${Outdir}/mutations.csv
+ done
