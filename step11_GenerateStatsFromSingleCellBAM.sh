0071fd8ed3ad0048a047e1965d3762f5b34d82bb
HumzaHemani <humzah@umich.edu>
2021-07-13 16:57:12 -0400

    Update step11_GenerateStatsFromSingleCellBAM.sh

--- step11_GenerateStatsFromSingleCellBAM.sh
+++ step11_GenerateStatsFromSingleCellBAM.sh
@@ -4,6 +4,7 @@
 Sample=${Sample:default_sample_num}
 DataDirectory=${DataDirectory:default_data_dir}
 Targets=${Targets:default_targets}
+Outdir=${Outdir:default_outdir}
 
 while [ $# -gt 0 ]; do            
     if [[ $1 == *"--"* ]]; then
@@ -27,4 +28,4 @@ COV_EXOME=$(samtools depth -b ${Targets} ${DataDirectory}/${Sample} | wc -l)
 echo cov_exome done
 COV=$(samtools depth ${DataDirectory}/${Sample} | wc -l)
 echo cov done
-echo $Sample, $READ_EXOME, $READ_SAM, $UMI, $COV_EXOME, $COV >> mutations.csv
+echo $Sample, $READ_EXOME, $READ_SAM, $UMI, $COV_EXOME, $COV >> ${Outdir}/mutations.csv
